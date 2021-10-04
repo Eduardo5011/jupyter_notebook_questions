45cc9fb0209a3b852819229c9237bd0a484e613c
Eduardo5011 <eduardo.rosado5011@gmail.com>
2021-10-04 13:38:09 -0400

    update

--- work.py
+++ work.py
@@ -1,41 +1,41 @@
-# # Question 1
-# def hello_name(user_name):
-# 	print("hello_" + user_name.upper() + "!")
+# Question 1
+def hello_name(user_name):
+	print("hello_" + user_name.upper() + "!")
 
-# hello_name('username')	
-
-
-# # # Question 2
-# odd_numbers = list(range(3,100,3))
-# print(odd_numbers)
+hello_name('username')	
 
 
-#  # Question 3
-# def max_num_in_list(a_list):
-#     max = a_list[0]
-#     for a in a_list:
-#         if a > max:
-#             max = a
-#     return max        
+# # Question 2
+odd_numbers = list(range(3,100,3))
+print(odd_numbers)
 
-# print(max_num_in_list([1, 2, 3, 4, 5]))
+
+ # Question 3
+def max_num_in_list(a_list):
+    max = a_list[0]
+    for a in a_list:
+        if a > max:
+            max = a
+    return max        
+
+print(max_num_in_list([1, 2, 3, 4, 5]))
 
             
 
 
-# # Question 4
-# def is_leap_year(a_year):
+# Question 4
+def is_leap_year(a_year):
 
-#     if a_year % 400 == 0:
-#         return True
-#     elif a_year % 100 != 0:
-#         return False
-#     elif a_year % 4 == 0:
-#         return True
+    if a_year % 400 == 0:
+        return True
+    elif a_year % 100 != 0:
+        return False
+    elif a_year % 4 == 0:
+        return True
 
-# print(is_leap_year(True))
-# print(is_leap_year(False))
-# print(is_leap_year(True))				
+print(is_leap_year(True))
+print(is_leap_year(False))
+print(is_leap_year(True))				
  				
 
 
